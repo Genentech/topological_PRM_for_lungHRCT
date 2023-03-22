9dcae28e1c5914d602ed6b11a8859aed97d5a1e1
bechtea2 <bechtel.aryil@gene.com>
2023-03-22 13:05:22 -0400

    make config file directories generic

--- config/write_config.py
+++ config/write_config.py
@@ -3,10 +3,10 @@ from configparser import ConfigParser
 
 config = ConfigParser()
 config["inFiles"] = {
-    "exp": "/Users/bechtea2/Documents/projects/COPD_longitudinal/COPDST2OP/data/sandbox/test_CODPGene_registered/10005Q/10005Q_EXP_STD_NJC_COPD.10005Q_INSP_B31f_330_NJC_COPD2.warped.nii.gz",
-    "insp_reg": "/Users/bechtea2/Documents/projects/COPD_longitudinal/COPDST2OP/data/sandbox/test_CODPGene_registered/10005Q/10005Q_EXP_STD_NJC_COPD.10005Q_INSP_B31f_330_NJC_COPD2.warped.nii.gz",
-    "mask": "/Users/bechtea2/Documents/projects/COPD_longitudinal/COPDST2OP/data/sandbox/test_CODPGene_registered/10005Q/10005Q_EXP_STD_NJC_COPD.njh.lung_segmentation.nii.gz",
+    "exp": "path/to/expiratory_image.nii.gz",
+    "insp_reg": "path/to/inspiratory_registered_image.nii.gz",
+    "mask": "path/to/mask.nii.gz",
 }
 
-with open("config.ini", "w") as f:
+with open("config/config.ini", "w") as f:
     config.write(f)
