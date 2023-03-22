7b989cedd311ac17774fb1aa6f93d4b825f08d40
Aryil Bechtel <bechtel.aryil@gene.com>
2023-03-22 17:41:14 +0100

    Upload img_utils.py

---
+++ utils/img_utils.py
@@ -0,0 +1,22 @@
+"""Utils for image processing."""
+import numpy as np
+import scipy
+
+
+def medFilt(image: np.array, kernelSize: int):
+    """Apply moving 2D median filter.
+
+    Args:
+        image (np.array): input image
+        kernelSize (int): size of moving window
+
+    Returns:
+        imageFilt (np.array): filtered image
+    """
+    imageFilt = np.zeros(image.shape)
+    for s in range(0, image.shape[0]):
+        imageFilt[s, :, :] = scipy.signal.medfilt2d(
+            image[s, :, :], kernel_size=kernelSize
+        )
+
+    return imageFilt
