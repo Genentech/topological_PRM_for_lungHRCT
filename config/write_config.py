94b14973d9a406af43acff85103b4446ced7f2c9
bechtea2 <bechtel.aryil@gene.com>
2023-04-06 14:32:53 -0400

    add batch processing functionality to main.py

--- config/write_config.py
+++ config/write_config.py
@@ -3,13 +3,13 @@ from configparser import ConfigParser
 
 config = ConfigParser()
 config.optionxform = str  # make keys case sensitive
-config["subjInfo"] = {"subjID": "000001"}
+config["subjInfo"] = {"subjID": "10161E_phase3"}
 config["io"] = {
-    "inFileExp": "path/to/expiratory_image.nii.gz",
-    "inFileInspReg": "path/to/inspiratory_registered_image.nii.gz",
-    "inFileMask": "path/to/mask.nii.gz",
-    "outDir": "path/to/main/out_directory/",
+    "inFileExp": "/Users/bechtea2/Documents/projects/COPD_longitudinal/COPDGene/data/sandbox/test_CODPGene_registered/10161E_phase3/10161E_EXP_STD_NJC_COPD.10161E_EXP_B31f_351_NJC_COPD3.warped.nii.gz",
+    "inFileInspReg": "/Users/bechtea2/Documents/projects/COPD_longitudinal/COPDGene/data/sandbox/test_CODPGene_registered/10161E_phase3/10161E_EXP_STD_NJC_COPD.10161E_INSP_B31f_351_NJC_LD_COPD3.warped.nii.gz",
+    "inFileMask": "/Users/bechtea2/Documents/projects/COPD_longitudinal/COPDGene/data/sandbox/test_CODPGene_registered/10161E_phase3/10161E_EXP_STD_NJC_COPD.njh.lung_segmentation.nii.gz",
+    "outDir": "/Users/bechtea2/Documents/projects/COPD_longitudinal/COPDGene/data/sandbox/test_CODPGene_registered/10161E_phase3",
 }
 
-with open("config/config_demo.ini", "w") as f:
+with open("config/config4.ini", "w") as f:
     config.write(f)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """Run HRCT PRM + topoligcal mapping pipeline."""
 import argparse
+import glob
 import logging
 from configparser import ConfigParser
+from os.path import join
 
 from subject_classmap import Subject
 
@@ -11,7 +13,17 @@ logging.basicConfig(level=logging.INFO)
 # set command line flags/args
 parser = argparse.ArgumentParser(description="Run HRCT voxel-wise lung image analysis")
 parser.add_argument(
-    "--config", type=str, metavar="", required=True, help="configuration file path"
+    "--config",
+    type=str,
+    metavar="",
+    required=True,
+    help="single subject: config file path. batch processing: directory of config files",
+)
+parser.add_argument(
+    "--batch",
+    action="store_true",
+    required=False,
+    help="indicate to run a batch process",
 )
 args = parser.parse_args()
 
@@ -42,14 +54,33 @@ def prmTopoMapping(config):
 
 
 def main():
-    """Run PRM and topological mapping HRCT analysis."""
+    """Run PRM and topological mapping HRCT analysis.
 
-    # read in config file
-    config = ConfigParser()
-    config.read(args.config)
+    If no batch processing indicated, run analysis with single config file.
+    If batch processing indicated, run analysis on each config file in specified directory.
+    """
 
-    # run pipeline from registered HRCT files
-    prmTopoMapping(config)
+    if not args.batch:
+        # read in config file
+        config = ConfigParser()
+        config.read(args.config)
+
+        # run pipeline from registered HRCT files
+        logging.info("*****Processing subject %s*****" % config["subjInfo"]["subjID"])
+        prmTopoMapping(config)
+    else:
+        # get list of config files in specified directory
+        configList = glob.glob(join(args.config, "*.ini"))
+        for configDir in configList:
+            # read in config file
+            config = ConfigParser()
+            config.read(configDir)
+
+            # run pipeline from registered HRCT files
+            logging.info(
+                "*****Processing subject %s*****" % config["subjInfo"]["subjID"]
+            )
+            prmTopoMapping(config)
 
 
 if __name__ == "__main__":
