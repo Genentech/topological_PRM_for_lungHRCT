8049e510f68d142e9b7dadb9204d8d9d8e890ebd
bechtea2 <bechtel.aryil@gene.com>
2023-04-27 14:34:43 -0700

    add colorbar to prm color png

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -19,8 +19,8 @@ def plotPrmRgbOnCt(
     """
 
     # create custom discrete colormap and bounds
-    cmap = colors.ListedColormap(["black", "green", "yellow", "red", "purple"])
-    bounds = [0, 1, 2, 3, 4, 5]
+    cmap = colors.ListedColormap(["green", "yellow", "red", "purple"])
+    bounds = [1, 2, 3, 4, 5]
     norm = colors.BoundaryNorm(bounds, cmap.N)
 
     # take single slice of PRM binned image and mask out un-binned regions
@@ -28,10 +28,12 @@ def plotPrmRgbOnCt(
     prmAllArraySlice = np.ma.masked_where(prmAllArraySlice < 1, prmAllArraySlice)
 
     # plot image overlaid on corresponding HRCT slice
-    plt.figure()
+    plt.subplots()
     plt.imshow(ctArray[:, sliceNum, :], cmap="gray")
     plt.imshow(prmAllArraySlice, cmap=cmap, norm=norm, alpha=0.7)
+    cbar = plt.colorbar(ticks=[1.5, 2.5, 3.5, 4.5], pad=0)
+    cbar.ax.set_yticklabels(["norm", "fSAD", "emph", "empt\nemph"])
     plt.axis("off")
-    plt.savefig(path, transparent=True, bbox_inches="tight", pad_inches=-0.05, dpi=600)
+    plt.savefig(path, transparent=True, bbox_inches="tight", dpi=600)
     plt.clf()
     plt.close()
