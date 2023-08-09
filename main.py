6824c933c6d6698c94ad06e568362db0ff31b68e
bechtea2 <bechtel.aryil@gene.com>
2023-08-09 01:16:04 -0400

    fix bug in call to genDictOfImageArrays

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def processSubject(config, args):
 
     # calculate global topology metrics and save topology stats
     logging.info("Calculating global topology metrics")
-    subject.genListOfImageArrays()
+    subject.genDictOfImageArrays()
     subject.calcTopologyGlobal()
     subject.saveTopologyStats()
 
